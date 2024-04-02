9428ff8d879caf4cde1882a300f77578aa717c21
renatoirina <irinarenato793@gmail.com>
2024-04-02 17:36:53 +0200

    feat: JSnack7 completed

---
+++ JSnack 7/script.js
@@ -0,0 +1,7 @@
+// Definisco la variabile "n"
+let n = 2;
+
+// Eseguo un ciclo for con la costante i=1000 e la variabile n stampa ad ogni ciclo il suo valore e poi si moltiplica per se stesso
+for(let i = 1000; n <= i; n*=2) {
+    console.log(n);
+}

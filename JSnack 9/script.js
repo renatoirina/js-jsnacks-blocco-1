f6613422388d36b87986f37c133ac50221e7cd7f
renatoirina <irinarenato793@gmail.com>
2024-04-03 23:03:16 +0200

    feat: JSnack9 completed

---
+++ JSnack 9/script.js
@@ -0,0 +1,14 @@
+// Inizializzo la variabile per la somma
+let somma = 0;
+
+// Calcolo la somma dei primi 10 numeri
+for (let i = 1; i <= 10; i++) {
+    somma += i;
+}
+
+// Calcolo la media
+const media = somma / 10;
+
+// Stampiamo la somma e la media
+console.log("La somma dei primi 10 numeri è:", somma);
+console.log("La media dei primi 10 numeri è:", media);
